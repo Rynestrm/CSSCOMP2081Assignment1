f4b45d2a14cef96b46ae06bc9a4ca2aa1cf6a6d6
Ivan <gibson.ivan@gmail.com>
2020-10-21 12:13:01 -0400

    added some JS, change background image, and restyled navbar

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+const navMenu = document.querySelector(".fa-bars");
+const overLay = document.querySelector("#overlay");
+const hamburger = document.querySelector(".fa-bars");
+const closeMenu = document.querySelector(".fa-times");
+
+
+function menuDisplay() {
+    overLay.classList.toggle('overlay');
+    overLay.classList.toggle('hidden');
+}
+
+navMenu.addEventListener('click', menuDisplay);
+overLay.addEventListener('click', menuDisplay);
